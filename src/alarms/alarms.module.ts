610b7963a676f8e5b4dd46e59766ebd5547e818f
Your New Name <nghiavu2k2abc@gmail.com>
2024-02-07 17:53:05 +0700

    update alarms.module.ts

--- src/alarms/alarms.module.ts
+++ src/alarms/alarms.module.ts
@@ -2,10 +2,12 @@ import { DynamicModule, Module, Type } from '@nestjs/common';
 import { AlarmsService } from './application/alarms.service';
 import { AlarmsController } from './presenters/http/alarms.controller';
 import { AlarmFactory } from './domain/factories/alarm.factory';
+import { CoreModule } from './core/core.module';
 
 @Module({
   controllers: [AlarmsController],
   providers: [AlarmsService, AlarmFactory],
+  imports: [CoreModule],
 })
 export class AlarmsModule {
   static withInfrastructure(infrastructureModule: Type | DynamicModule) {

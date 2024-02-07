47a40a96d6614084824196ee164e0c683d776036
Your New Name <nghiavu2k2abc@gmail.com>
2024-02-07 17:48:30 +0700

    update alarms.service.ts

--- src/alarms/application/alarms.service.ts
+++ src/alarms/application/alarms.service.ts
@@ -2,14 +2,24 @@ import { Injectable } from '@nestjs/common';
 import { CreateAlarmDto } from '../presenters/http/dto/create-alarm.dto';
 import { UpdateAlarmDto } from '../presenters/http/dto/update-alarm.dto';
 import { CreateAlarmCommand } from './commands/create-alarm.command';
+import { AlarmRepository } from './ports/alarms.repository';
+import { AlarmFactory } from '../domain/factories/alarm.factory';
+
 
 @Injectable()
 export class AlarmsService {
-  create(createAlarmDto: CreateAlarmCommand) {
-    return 'This action adds a new alarm';
+  constructor(
+    private readonly AlarmRepository: AlarmRepository,
+    private readonly AlarmFactory: AlarmFactory,
+  ) {}
+  create(createAlarmCommand: CreateAlarmCommand) {
+
+
+    const alarm = this.AlarmFactory.create(createAlarmCommand.name, createAlarmCommand.severity)
+    return this.AlarmRepository.save(alarm)
   }
 
   findAll() {
-    return `This action returns all alarms`;
+    return  this.AlarmRepository.findAll()
   }
 }

74d31a80af069a38f1ec440a6ccff65bec77486d
Your New Name <nghiavu2k2abc@gmail.com>
2024-02-07 17:24:15 +0700

    1

---
+++ src/alarms/infrastructure/presenters/orm/orm-persistence.module.ts
@@ -0,0 +1,14 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { AlarmRepository } from "src/alarms/application/ports/alarms.repository";
+import { AlarmEntity } from "./entities/alarm.entity";
+import { OrmAlarmRepository } from "./repositories/alarms.repository";
+
+@Module({
+  imports: [TypeOrmModule.forFeature([AlarmEntity])],
+  providers: [{
+    provide: AlarmRepository,
+    useClass: OrmAlarmRepository
+  }],exports: [AlarmRepository]
+})
+export class OrmAlarmPersistenceModule {}
